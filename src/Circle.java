35cedf904cdc6714bda36f1a9712f8e415f5e659
Gulshan <shana_1991@list.ru>
2023-01-21 02:01:27 +0300

    https://github.com/medertynychbekov/ConstructorPractice.git

---
+++ src/Circle.java
@@ -0,0 +1,11 @@
+public class Circle {
+    private  static  final double PI=3.14;
+    public  static double area (double radius){
+        return PI*(radius*radius);
+
+    }
+    public  static double circumference( double radius){
+        return PI*2*radius;
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double radi=5.5;
+        System.out.println( Circle.area(radi));
+        System.out.println(Circle.circumference(radi));
+
+
+    }
+}
